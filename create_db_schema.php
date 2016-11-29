a637c6f1822e18bbcba5d310bb3888dd117d45a0
Priyanka Reddy <preddy@box.com>
2016-11-28 23:16:23 -0800

    Add db_schema file

---
+++ create_db_schema.php
@@ -0,0 +1,28 @@
+<?php
+
+connectToDb();
+function connectToDb() {
+    $dbopts = parse_url(getenv('DATABASE_URL'));
+    println($dbopts);
+
+    $servername = $dbopts["host"];
+    $username = $dbopts["user"];
+    $password = $dbopts["pass"];
+    $port = $dbopts["port"];
+    $dbName = $dbopts["path"];
+
+    try {
+        $conn = new PDO("mysql:host=$servername;dbname=$dbName;port=$port", $username, $password);
+        // set the PDO error mode to exception
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        echo"Connected successfully";
+    }
+    catch(PDOException $e)
+    {
+        echo"Connection failed: " . $e->getMessage();
+    }
+}
+
+
+
+?>
