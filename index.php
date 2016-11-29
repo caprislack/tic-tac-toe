440e636cf76d378bfbe47dbb08b32d306f150ec3
Priyanka Reddy <preddy@box.com>
2016-11-29 01:25:56 -0800

    Main game

--- index.php
+++ index.php
@@ -17,7 +17,6 @@ function connectToDb() {
 
     try {
         $conn = new mysqli($servername, $username, $password, $dbName);        // set the PDO error mode to exception
-        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         //echo"Connected successfully";
     }
     catch(PDOException $e)
