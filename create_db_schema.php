440b0c689599c56c4cb4a79362fc1dcb6e7855d7
Priyanka Reddy <preddy@box.com>
2016-11-29 00:05:15 -0800

    Create_db_schema.php

--- create_db_schema.php
+++ create_db_schema.php
@@ -11,16 +11,16 @@ function connectToDb() {
     $port = $dbopts["port"];
     $dbName = $dbopts["path"];
 
-    try {
-        $conn = new PDO("mysql:host=$servername;dbname=$dbName;port=$port", $username, $password);
-        // set the PDO error mode to exception
-        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        echo"Connected successfully";
-    }
-    catch(PDOException $e)
-    {
-        echo"Connection failed: " . $e->getMessage();
-    }
+//    try {
+//        $conn = new PDO("mysql:host=$servername;dbname=$dbName;port=$port", $username, $password);
+//        // set the PDO error mode to exception
+//        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+//        echo"Connected successfully";
+//    }
+//    catch(PDOException $e)
+//    {
+//        echo"Connection failed: " . $e->getMessage();
+//    }
 }
 
 
