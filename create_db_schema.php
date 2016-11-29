b8357b5c7a79b37980b4cd8f99dde2633ec0a932
Priyanka Reddy <preddy@box.com>
2016-11-29 00:31:43 -0800

    create_db_schema

--- create_db_schema.php
+++ create_db_schema.php
@@ -16,6 +16,18 @@ function connectToDb() {
         // set the PDO error mode to exception
         $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         echo"Connected successfully";
+        $conn->exec("CREATE TABLE `open_games` (
+  `team_id` varchar(11) NOT NULL,
+  `team_name` varchar(100) NOT NULL,
+  `channel_id` varchar(11) NOT NULL,
+  `channel_name` varchar(100) NOT NULL,
+  `initiating_user` varchar(100) NOT NULL,
+  `initiating_user_name` varchar(100) NOT NULL,
+  `other_user` varchar(100) NOT NULL,
+  `other_user_name` varchar(100) NOT NULL,
+  `current_player` tinyint(4) NOT NULL,
+  `board` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;");
     }
     catch(PDOException $e)
     {
